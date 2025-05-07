8f8982c86a3062380b124b9d3ee9fd442a9e6e54
Yashab Alam <77148571+yashab-cyber@users.noreply.github.com>
2025-05-07 05:35:21 +0530

    Delete backend directory

--- backend/routes/contact.js
+++
@@ -1,9 +0,0 @@
-import express from "express";
-import { submitContactForm } from "../controllers/contactController.js";
-
-const router = express.Router();
-
-// POST /api/contact
-router.post("/", submitContactForm);
-
-export default router;
